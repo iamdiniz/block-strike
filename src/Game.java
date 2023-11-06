464d77bbb023e9e41dfc4c795005716ad04a0d0b
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-06 16:03:57 -0300

    Creating game window with JFrame

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,47 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-public class Game {
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	public Game() {
+		Dimension dimension = new Dimension(640, 480);
+		this.setPreferredSize(dimension);
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		JFrame jframe = new JFrame("My game");
+		jframe.add(game);
+		jframe.setLocationRelativeTo(null);
+		jframe.pack();
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		jframe.setVisible(true);
+	}
+	
+	@Override
+	public void run() {
+		
+		while(true) {
+			update();
+			render();
+			try {
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 
 }
