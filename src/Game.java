2d01deb9ab619ee756fd5b85415360749be89373
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-07 14:56:54 -0300

    Creating Life Bar

--- src/Game.java
+++ src/Game.java
@@ -12,16 +12,23 @@ public class Game extends Canvas implements Runnable {
 	
 	public static final int HEIGHT = 480;
 	
+	public int barraDeVida = 100;
+	
 	public Game() {
 		Dimension dimension = new Dimension(WIDTH, HEIGHT);
 		this.setPreferredSize(dimension);
 	}
 	
 	public void update() {
+		barraDeVida--;
+		if (barraDeVida <= 0) {
+			// Aqui seria o Game Over
+			barraDeVida = 100;
+		}
 	}
 	
 	public void render() {
-		BufferStrategy bs = this.getBufferStrategy();
+		BufferStrategy bs = this.getBufferStrategy(); // Vamos usar o double buffering
 		
 		if (bs == null) { // Caso não exista
 			this.createBufferStrategy(3); // Crie uma
@@ -32,6 +39,19 @@ public class Game extends Canvas implements Runnable {
 		graphics.setColor(Color.black);
 		graphics.fillRect(0, 0, WIDTH, HEIGHT);
 		
+		
+		/*
+		graphics.setColor(Color.white);
+		graphics.setFont(new Font("Arial", Font.BOLD, 23));
+		
+		graphics.drawString("Pontos: " + contador, WIDTH/2 - 60, 30);
+		
+		*/
+		
+		graphics.setColor(Color.green);
+		graphics.fillRect(Game.WIDTH/2 - 100 - 70, 20, barraDeVida*3, 20);
+		graphics.setColor(Color.white);
+		graphics.drawRect(Game.WIDTH/2 - 100 - 70, 20, 300, 20);
 		bs.show();
 		
 	}
